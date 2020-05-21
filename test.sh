c893ba9dbe11b443e8268f756a08daffee7f24ae
Julia Paglia <33559206+jpaglia@users.noreply.github.com>
2020-05-21 13:01:30 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,7 @@
 #!/bin/bash -e
 
 # Virtual Memory Unit Compilation and Execution Script
-# EECS 3221 Operating System Concepts
-# Mini Project 3
 # Author: Julia Paglia
-
-# I verify that this work is my own. However, I will disclose that some conversations were had with students
-# Amer Alshoghri and Daniel Santaguida about general troubleshooting and debugging, and some source code for
-# the linked list LRU implementation was adapted from my Mini Project 2.
 
 echo "Compiling"
 gcc mmu.c -o mmu
